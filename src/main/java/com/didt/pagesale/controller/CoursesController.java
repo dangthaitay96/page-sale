a37769e57ebd12d1a9c0e342813faff7c48dfedb
taydt <dangthaitay96@gmail.com>
2024-06-24 01:54:51 +0700

    .

--- src/main/java/com/didt/pagesale/controller/CoursesController.java
+++ src/main/java/com/didt/pagesale/controller/CoursesController.java
@@ -3,57 +3,66 @@ package com.didt.pagesale.controller;
 import com.didt.pagesale.dto.CoursesDto;
 import com.didt.pagesale.response.FileUploadResponse;
 import com.didt.pagesale.server.CoursesService;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
+import org.springframework.web.util.UriUtils;
 
 @RestController
 @RequestMapping("/courses")
 public class CoursesController {
-    private final CoursesService coursesService;
+  private final CoursesService coursesService;
 
-    public CoursesController(CoursesService coursesService) {
-        this.coursesService = coursesService;
-    }
+  public CoursesController(CoursesService coursesService) {
+    this.coursesService = coursesService;
+  }
 
-    @GetMapping("/listCourses")
-    public ResponseEntity<List<CoursesDto>> getCourses() {
-        return new ResponseEntity<>(coursesService.getCourses(), HttpStatus.OK);
-    }
+  @GetMapping("/listCourses")
+  public ResponseEntity<List<CoursesDto>> getCourses() {
+    return new ResponseEntity<>(coursesService.getCourses(), HttpStatus.OK);
+  }
 
-    @PostMapping("/create")
-    public ResponseEntity<FileUploadResponse> createCourses(
-            @RequestParam("file") MultipartFile multipartFile,
-            @RequestParam String description,
-            @RequestParam String name) throws IOException {
-        return new ResponseEntity<>(coursesService.upload(multipartFile, description, name), HttpStatus.OK);
-    }
+  @PostMapping("/create")
+  public ResponseEntity<FileUploadResponse> createCourses(
+      @RequestParam("file") MultipartFile multipartFile,
+      @RequestParam String description,
+      @RequestParam String name)
+      throws IOException {
+    return new ResponseEntity<>(
+        coursesService.upload(multipartFile, description, name), HttpStatus.OK);
+  }
 
-    @PutMapping("/update")
-    public ResponseEntity<?> update(
-            @RequestParam(value = "file", required = false) MultipartFile multipartFile,
-            @RequestParam String description,
-            @RequestParam Long id,
-            @RequestParam String name) throws IOException {
-        return new ResponseEntity<>(coursesService.update(id, multipartFile, description, name), HttpStatus.OK);
-    }
+  @PutMapping("/update")
+  public ResponseEntity<?> update(
+      @RequestParam(value = "file", required = false) MultipartFile multipartFile,
+      @RequestParam String description,
+      @RequestParam Long id,
+      @RequestParam String name)
+      throws IOException {
+    return new ResponseEntity<>(
+        coursesService.update(id, multipartFile, description, name), HttpStatus.OK);
+  }
 
-    @GetMapping("/files/{filename:.+}")
-    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
-        Resource file = coursesService.load(filename);
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
+  @GetMapping("/files/{filename:.+}")
+  public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+    Resource file = coursesService.load(filename);
+    return ResponseEntity.ok()
+        .header(
+            HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=\""
+                + UriUtils.encode(file.getFilename(), StandardCharsets.UTF_8)
+                + "\"")
+        .body(file);
+  }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Long> deleteCourses(@PathVariable(value = "id") Long id) {
-        return new ResponseEntity<>(coursesService.delete(id), HttpStatus.OK);
-    }
-
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Long> deleteCourses(@PathVariable(value = "id") Long id) {
+    return new ResponseEntity<>(coursesService.delete(id), HttpStatus.OK);
+  }
 }
